52797bcdd66c471d74968c30de916457c1523808
Abigail McFarlane <abigail.mcfarlane@mymona.uwi.edu>
2022-11-12 11:27:12 -0500

    Display cities

--- world.js
+++ world.js
@@ -1,11 +1,29 @@
 window.onload = function(){
     let countrybtn = document.getElementById("lookup");
-
+    
     countrybtn.onclick = function(){
         let country = document.getElementById("country").value;
         let result = document.getElementById("result");
 
         fetch(`http://localhost/info2180-lab5/world.php?country=${country.trim()}`)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            result.innerHTML = data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    let citybtn = document.getElementById("lookup_cities");
+
+    citybtn.onclick = function(){
+        let country = document.getElementById("country").value;
+        let result = document.getElementById("result");
+
+        fetch(`http://localhost/info2180-lab5/world.php?country=${country.trim()}&lookup=cities`)
         .then(response => {
             return response.text();
         })
