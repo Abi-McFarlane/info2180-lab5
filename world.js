07f069f4f0fbb51e7f50756d2b92dbcdc9f3839d
Abigail McFarlane <abigail.mcfarlane@mymona.uwi.edu>
2022-11-12 10:08:34 -0500

    Look up any country

---
+++ world.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    let countrybtn = document.getElementById("lookup");
+
+    countrybtn.onclick = function(){
+        let country = document.getElementById("country").value;
+        let result = document.getElementById("result");
+
+        fetch(`http://localhost/info2180-lab5/world.php?country=${country.trim()}`)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            result.innerHTML = data;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+}
